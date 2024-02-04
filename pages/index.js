ee3b8014239720e69bfb7847abb95ab6ab7398b6
npagolu <npagolu@altimetrik.com>
2024-02-04 14:07:57 +0530

    Added chrome API's and realted UI

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,6 @@ export default function Home() {
 
   return (
     <div className="container">
-      <Head>
       <ul>
         <li>
           Home
@@ -35,16 +34,12 @@ export default function Home() {
           About Us
         </li>
       </ul>
-      </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-
-      <Footer />
     </div>
   )
 }
