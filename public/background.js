1abe4c6137622ceecf5656c7351e14e8ced64ba8
npagolu <npagolu@altimetrik.com>
2024-02-04 14:34:52 +0530

    Added chrome API's and realted UI

--- public/background.js
+++ public/background.js
@@ -1,5 +1,19 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {debugger
-    if (request.action === 'highlightElement') {
-      console.log('Received message:', request.element);
+
+const highlightedElements = [];
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'highlightElement') {
+    highlightedElements.push(request.element);
+    chrome.runtime.sendMessage({ action: 'updateList', elements: highlightedElements });
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'deleteElement') {
+    const index = highlightedElements.indexOf(request.element);
+    if (index !== -1) {
+      highlightedElements.splice(index, 1);
+      chrome.runtime.sendMessage({ action: 'updateList', elements: highlightedElements });
     }
+  }
 });
